8c49f919c80fe9d71e393b04300ca4e2c8f1d40a
Briandepalmas <Student_GA110@Student-GA110.home>
2020-03-27 22:20:39 -0400

    added buttons and created state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+                seconds: 0,
+                minutes: 0,
+                pause: false
+
+    }
+
+  }
   render() {
     return (
       <div>
-        
+        <button>Start</button>
+        <button>Pause</button>
+        <button>Reset</button>
       </div>
     )
   }
