e905fe1c2830eb4ec46a5edaf6db0598c1c488d0
Briandepalmas <Student_GA110@Student-GA110.home>
2020-03-27 23:10:58 -0400

    connected buttons to function

--- src/App.js
+++ src/App.js
@@ -13,7 +13,13 @@ export class App extends Component {
   }
 
   handleStartClick(){
+    //setInterval calls the timer function every second.
+    this.startTimer=setInterval(this.timer,1000)
       
+  }
+  timer(){
+
+    this.setState({seconds: this.state.seconds + 1})
   }
 
   handlePauseClick(){
@@ -28,10 +34,10 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <h1>0:00</h1>
-        <button>Start</button>
-        <button>Pause</button>
-        <button>Reset</button>
+        <h1>{this.state.seconds}</h1>
+        <button onClick={this.handleStartClick}>Start</button>
+        <button onClick={this.handlePauseClick}>Pause</button>
+        <button onClick={this.handleResetClick}>Reset</button>
       </div>
     )
   }
