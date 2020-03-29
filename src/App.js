c56abd15bf84b0512ac94acdfc36f8adf55c2081
Briandepalmas <Student_GA110@Student-GA110.home>
2020-03-28 23:01:37 -0400

    final touches and pseudo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+// App.css does not appear when typing './' 
+//typing it manually does not import App.css
+//added css natively in react instead
 import './App.css'
 
 export class App extends Component {
@@ -10,6 +13,8 @@ export class App extends Component {
                 minutes: 0,
                 pause: false
     }
+
+    //binding the word this to the function allows to use the functions on the buttons 
     this.timer=this.timer.bind(this)
     this.handleStartClick=this.handleStartClick.bind(this)
     this.handlePauseClick=this.handlePauseClick.bind(this)
@@ -22,9 +27,9 @@ export class App extends Component {
     this.startTimer=setInterval(this.timer,1000)
       
   }
-  
+  //this function is the one that adds 1 to "seconds" property every second.
   timer(){
-    if (this.state.pause==false){
+    if (this.state.pause===false){
         this.setState({seconds: this.state.seconds + 1})
       }   
   }
@@ -33,9 +38,12 @@ export class App extends Component {
    
   handlePauseClick(){
     //switches the pause boolean between true and false onclick
+    //if pause is true, clearinterval stops the variable assigned to setInterval from updating
+    //if pause is toggle back to false it calls the same variable assigned to setInterval that is found in the start button
+    //toggle code learned and implemented from Stack Overflow:https://stackoverflow.com/questions/40359800/how-to-toggle-boolean-state-of-react-component
     this.setState(pauseSwitch => ({pause: !pauseSwitch.pause}));
    
-  if (this.state.pause==true){
+  if (this.state.pause===true){
       clearInterval(this.startTimer)
       }
       else {
@@ -45,21 +53,33 @@ export class App extends Component {
   }
   
   
-  
+  //When this function is called it resets the time by setting the seconds property to 0 
+  //at the same time it stops the timer function from adding 1 to the seconds property
   handleResetClick(){
     this.setState({seconds: 0});
     clearInterval(this.timer);
   }
 
 
+//buttons labeled to corresponding function
 
   render() {
+//Import App.css didnt work so i added some style natively
+    const style={
+      height:"50px",
+     width:"100px",
+     backgroundColor: "brown",
+     color:"yellow",
+     fontSize:"20px"
+ }
+
+    
     return (
       <div>
         <h1>{this.state.seconds}</h1>
-        <button onClick={this.handleStartClick}>Start</button>
-        <button onClick={this.handlePauseClick}>Pause</button>
-        <button onClick={this.handleResetClick}>Reset</button>
+        <button style={style}className="button"onClick={this.handleStartClick}>Start</button>
+        <button style={style}className="button"onClick={this.handlePauseClick}>Pause</button>
+        <button style={style}className="button"onClick={this.handleResetClick}>Reset</button>
       </div>
     )
   }
